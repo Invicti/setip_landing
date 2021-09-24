116bd108f6e612553dd26ab4822a46dd8a346c20
Nicolas Fodor <nicolas@fodor.net>
2021-09-24 15:48:50 -0700

    auto-commit runlocal.sh

--- builddocker.sh
+++ builddocker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MAIN_TAG="0.1.0"
+MAIN_TAG="0.1.1"
 TAG="${MAIN_TAG:-latest}"
 IMAGE="live-reload"
 REPO="invictieu"
@@ -15,7 +15,7 @@ docker push "${REPO}/${IMAGE}:$TAG"
 docker rm /node-app -f
 docker run -it  \
 --name ${DOCKER_APP_NAME} \
--p 173.21.4.2:3001:3000 \
+-p 173.21.4.2:3000:3000 \
 -p 173.21.4.2:4500:4500  \
 -e GITHUB_URL=${GITHUB_URL}  \
 -e GITHUB_URL=${GITHUB_URL}  \
